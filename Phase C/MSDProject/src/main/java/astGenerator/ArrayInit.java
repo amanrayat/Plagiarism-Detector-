44b52916e404d554ae406296f69f7cef74035965
Harshmeet <johal.h@husky.neu.edu>
2018-02-21 23:51:49 -0500

    generated ast

---
+++ Phase C/MSDProject/src/main/java/astGenerator/ArrayInit.java
@@ -0,0 +1,9 @@
+package astGenerator;
+import org.antlr.v4.runtime.*;
+import org.antlr.v4.runtime.tree.*;
+ 
+public class ArrayInit {
+	public static void main(String[] args) {
+		
+	}
+}
---
+++ Phase C/MSDProject/src/main/java/astGenerator/ParserFacade.java
@@ -0,0 +1,38 @@
+package astGenerator;
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+
+import org.antlr.v4.runtime.ANTLRInputStream;
+import org.antlr.v4.runtime.CommonTokenStream;
+
+public class ParserFacade {
+    private static String readFile(File file, Charset encoding) throws IOException {
+        byte[] encoded = Files.readAllBytes(file.toPath());
+        return new String(encoded, encoding);
+    }
+ 
+    	
+ /**
+  * 
+  * @param file
+  * @return A parsed tree of type Python3Parser.File_inputContext
+  * @throws IOException
+  */
+    public Python3Parser.File_inputContext parse(File file) throws IOException {
+        String code = readFile(file, Charset.forName("UTF-8"));
+        Python3Lexer lexer = new Python3Lexer(new ANTLRInputStream(code));
+        CommonTokenStream tokens = new CommonTokenStream(lexer);
+        Python3Parser parser = new Python3Parser(tokens);
+        new AstPrinter().print(parser.file_input());
+        return parser.file_input();
+    }
+//}
+//    public static void main(String[] args) throws IOException {
+//		ParserFacade p = new ParserFacade();
+//		p.parse(new File("/Users/harshmeet/Desktop/test.py"));
+//	}
+	}
+	
+
