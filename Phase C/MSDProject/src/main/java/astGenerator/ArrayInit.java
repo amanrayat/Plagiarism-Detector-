fd460be3302fc06d117c330065c532c64a15f4d5
vihabidre <vihabidre@ccs.neu.edu>
2018-03-07 20:23:50 -0500

    Deleting folder Phase C

--- Phase C/MSDProject/src/main/java/astGenerator/ArrayInit.java
+++
@@ -1,9 +0,0 @@
-package astGenerator;
-import org.antlr.v4.runtime.*;
-import org.antlr.v4.runtime.tree.*;
- 
-public class ArrayInit {
-	public static void main(String[] args) {
-		
-	}
-}
--- Phase C/MSDProject/src/main/webapp/index.jsp
+++
@@ -1,5 +0,0 @@
-<html>
-<body>
-<h2 align="center">Welcome to Plagiarism Detector by Team-208!</h2>
-</body>
-</html>
