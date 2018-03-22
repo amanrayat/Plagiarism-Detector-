44a3ccf2b01157adc9b3fda596045e1fbdb9ce03
Harshmeet <johal.h@husky.neu.edu>
2018-03-21 23:38:45 -0400

    changes

--- PhaseC/PlagiarismDetector/src/main/java/com/team208/detector/FileCompareUtility.java
+++ PhaseC/PlagiarismDetector/src/main/java/com/team208/detector/FileCompareUtility.java
@@ -10,18 +10,20 @@ public class FileCompareUtility {
     	
         if (! md5EncoderUtil.encodeToMd5(actualFilePath).equals(md5EncoderUtil.encodeToMd5(expectedFilePath))){
         	    
-        	ExecuteShellComand e=new ExecuteShellComand();
+        	System.out.println("not same!");
+//        	ExecuteShellComand e=new ExecuteShellComand();
+        	
         	
         } else {
             System.out.println("The files- "+actualFilePath+" and "+expectedFilePath+" are identical");
         }
     }
 
-//    public static void main(String[] args) throws Exception {
-//    	String actualFileComparePath =  "/Users/harshmeet/Desktop/jplag/jplag_/directory_section01/Student1/tictactoe.py";
-//    	String expectedFileComparePath =  "/Users/harshmeet/Desktop/jplag/jplag_/directory_section01/Student1/tictactoe.py";
-//    	FileCompareUtility fileCompareUtil= new FileCompareUtility();
-//    	System.out.println(fileCompareUtil.compare2Files(actualFileComparePath, expectedFileComparePath));
-//
-//    }
+    public static void main(String[] args) throws Exception {
+    	String actualFileComparePath =  "/Users/harshmeet/Desktop/jplag/jplag_/directory_section01/Student1/tictactoe.py";
+    	String expectedFileComparePath =  "/Users/harshmeet/Desktop/jplag/jplag_/directory_section01/Student1/tictactoe.py";
+    	FileCompareUtility fileCompareUtil= new FileCompareUtility();
+    	fileCompareUtil.compare2Files(actualFileComparePath, expectedFileComparePath);
+
+    }
 }
