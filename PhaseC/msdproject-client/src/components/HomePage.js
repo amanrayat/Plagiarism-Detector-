d6f15dabf74b7063cf596f0043371a5b1b37d26e
vihabidre <vihabidre@ccs.neu.edu>
2018-03-22 02:31:32 -0400

    Adding bootstrap and a few UI pages

---
+++ PhaseC/msdproject-client/src/components/HomePage.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class HomePage extends React.Component{
+  render(){
+    return (
+      <div>
+        <h1> Plagiarism Detection System </h1>
+        <input type="text" name="email" placeholder="Email Address" />
+        <br />
+        <br />
+        <input type="password" name="password" placeholder="Password" />
+        <br />
+        <br />
+        <button> Login </button>
+        <button> Register </button>
+      </div>
+    );
+  }
+}
+
+export default HomePage
