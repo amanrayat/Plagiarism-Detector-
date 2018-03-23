143231991b01252bb57ce270055b06cf0ac225eb
rachanatondare <tondare.r@husky.neu.edu>
2018-03-23 18:28:02 -0400

    added tests and checked in surefire in jenkinsfile
    added tests and checked in surefire in jenkinsfile

---
+++ PhaseC/PlagiarismDetector/src/main/java/com/team208/jsonResponse/StudentLoginResponseBean.java
@@ -0,0 +1,33 @@
+package com.team208.jsonResponse;
+
+import java.io.Serializable;
+
+public class StudentLoginResponseBean implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	
+	private StatusBean status;
+	
+	private StudentEntityResponse studentEntity;
+
+	public StatusBean getStatus() {
+		return status;
+	}
+
+	public void setStatus(StatusBean status) {
+		this.status = status;
+	}
+
+	public StudentEntityResponse getStudentEntity() {
+		return studentEntity;
+	}
+
+	public void setStudentEntity(StudentEntityResponse studentEntity) {
+		this.studentEntity = studentEntity;
+	}
+	
+}
