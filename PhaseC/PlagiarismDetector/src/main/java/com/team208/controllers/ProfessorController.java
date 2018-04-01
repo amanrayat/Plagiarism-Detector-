500633b9b706b44d847bbdecce6f7df536acd196
Harshmeet <johal.h@husky.neu.edu>
2018-04-01 19:13:58 -0400

    returning plagiarism data for each student

--- PhaseC/PlagiarismDetector/src/main/java/com/team208/controllers/ProfessorController.java
+++ PhaseC/PlagiarismDetector/src/main/java/com/team208/controllers/ProfessorController.java
@@ -31,66 +31,40 @@ public class ProfessorController {
 
 	@Autowired 
 	private CourseRepository courseRepository;
-
 	@RequestMapping("/generateReport")
 	@ResponseBody
 	public String generateReport() throws IOException {
-
 		String gitLink = "https://github.com/amanrayat/testRepo.git";
 		List<String> hwlist = new ArrayList<>();
-
 		hwlist.add("homework1");
-
 		hwlist.add("homework2");
-
 		hwlist.add("homework3");
-
 		HashMap<String, String> studentRepo = new HashMap<>();
-
 		studentRepo.put("student001", gitLink);
-
 		studentRepo.put("student002", "https://github.com/enrolled02/python-crawler");
-
 		studentRepo.put("student003", gitLink);
-
 		studentRepo.put("student004", "https://github.com/enrolled02/python-crawler");
-
 		String hw;
-
 		String course;
-
 		List<String> courses = new ArrayList<>();
-
 		courses.add("CS5500");
-
 		courses.add("CS6200");
-
 		courses.add("CS5800");
-
 		for(int k =0; k<courses.size();k++) {
-
 			course = courses.get(k);
-
 			for(int j=0;j<hwlist.size();j++) {
-
 				hw = hwlist.get(j);
-
 				for(Map.Entry<String, String> entry: studentRepo.entrySet())
-
 					try {
 						GitRepoDownload.downloadRepo(course,hw,entry.getKey(),entry.getValue());
-
 					} catch (IOException e) {
 						LOGGER.info("Context : "+e.getMessage());
 					}
 			}
 
 		}
-
 		return ExecuteShellComand.getComparison("CS5500","homework1",75.0d); 
-
 	}
-
 	@RequestMapping("/deletCourse")
 	public @ResponseBody  StatusBean deleteCourse(@RequestParam int courseId){
 		StatusBean status = new StatusBean();
--- PhaseC/PlagiarismDetector/src/main/java/com/team208/detector/ReportGenerator.java
+++ PhaseC/PlagiarismDetector/src/main/java/com/team208/detector/ReportGenerator.java
@@ -2,6 +2,10 @@ package com.team208.detector;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeMap;
+
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
@@ -12,9 +16,30 @@ import org.jsoup.select.Elements;
  *
  */
 public class ReportGenerator {
-	
+
+	protected static Map<String, Double>avergeSimilaritystudentMap=new TreeMap<>();
+	protected static Map<String, Double>maxSimilaritystudentMap=new TreeMap<>();
+
+
 	private ReportGenerator() {
 		super();
+	}
+
+
+
+	public static void getAllStudents() throws IOException {
+		String	content = new String ( Files.readAllBytes( Paths.get("/Users/harshmeet/Desktop/untitledfolder3/results/index.html").toAbsolutePath() ) );
+		Document doc=Jsoup.parse(content);
+		doc.select("img").remove();
+		Elements rows=doc.select("table").get(0).select("tr");
+		Element row=rows.get(2);
+		Elements td=row.select("td");
+		Elements students=td.select("code");
+		String[] s=students.text().split(" - ");
+		for(int j=0;j<s.length;j++) {
+			avergeSimilaritystudentMap.put(s[j], 0.0d);
+			maxSimilaritystudentMap.put(s[j], 0.0d);
+		}
 	}
 	/**
 	 * 
@@ -23,8 +48,11 @@ public class ReportGenerator {
 	 * @throws IOException
 	 */
 	public static String  setThreshold(Double threshold) throws IOException  {
-		String	content = new String ( Files.readAllBytes( Paths.get("/Users/harshmeet/untitledfolder3/-target/results/index.html").toAbsolutePath() ) );
+		getAllStudents();
+		String	content = new String ( Files.readAllBytes( Paths.get("/Users/harshmeet/Desktop/untitledfolder3/results/index.html").toAbsolutePath() ) );
+
 		Document doc=Jsoup.parse(content);
+		doc.select("img").remove();
 		content = doc.html();
 		Element table1=doc.select("table").get(2);
 		Element table2=doc.select("table").get(3);
@@ -35,16 +63,15 @@ public class ReportGenerator {
 		for(int i=0;i<rows.size();i++) {
 			Element row = rows.get(i);
 			Elements cols = row.select("td");
-
-
 			for(int j=2;j<cols.size();j++) {
 				Element e = cols.get(j);
+				String studentID=e.select("a").text();
 				Elements thresholdString=e.select("font");
 				Double thresholdFromFile = Double.parseDouble(thresholdString.text().replaceAll("[()%]",""));
+				Double studentScore = Math.max(thresholdFromFile, avergeSimilaritystudentMap.getOrDefault(studentID, 0.0d));
+				avergeSimilaritystudentMap.put(studentID,studentScore);
 				if (thresholdFromFile >= threshold) {
-					//cols.remove(e);
-//					e.append("    ");
-					String temp ="<bgcolor=\"#FF0000\">"+ "<b>"+e.html()+ "</b>";
+					String temp ="<td bgcolor= #FFFF00\">"+ "<font color=\"#fff\">" + "<b>"+e.html()+ "</b>"+"</font>"+"</td>";
 					content = content.replace(e.html(), temp);
 				}
 			}
@@ -54,25 +81,31 @@ public class ReportGenerator {
 			Elements cols2=row2.select("td");
 			for(int k=2;k<cols2.size();k++) {
 				Element e1=cols2.get(k);
+				String studentID1=e1.select("a").text();
 				Elements thresholdedString=e1.select("font");
 				Double thresholdedFromFile1 = Double.parseDouble(thresholdedString.text().replaceAll("[()%]",""));
+				Double maxStudentScore=Math.max(thresholdedFromFile1, maxSimilaritystudentMap.getOrDefault(studentID1, 0.0d));
+				maxSimilaritystudentMap.put(studentID1,maxStudentScore);
 				if (thresholdedFromFile1 >= threshold) {
-					String temp ="<bgcolor=\"#FF0000\">"+ "<b>"+e1.html()+ "</b>";
+					String temp ="<td bgcolor= #FFFF00\">"+ "<font color=\"#fff\">" + "<b>"+e1.html()+ "</b>"+"</font>"+"</td>";
 					content = content.replace(e1.html(), temp);
 
-//					cols2.remove(e1);
 				}
 			}
 		}
+
 		content= content.replace(tempTable.html(), table1.html());
 		content = content.replace(tempTable2.html(), table2.html());
 		return content;
 	}
 
-	public static void main(String[] args) throws IOException {
-		ReportGenerator gen =new ReportGenerator();
-		System.out.println(gen.setThreshold(86.0d));
-
-	}
+//	public static void main(String[] args) throws IOException {
+//		ReportGenerator gen =new ReportGenerator();
+//		System.out.println(gen.setThreshold(3.0d));
+//		System.out.println(avergeSimilaritystudentMap);
+//		System.out.println(maxSimilaritystudentMap);
+//
+//
+//	}
 
 }
