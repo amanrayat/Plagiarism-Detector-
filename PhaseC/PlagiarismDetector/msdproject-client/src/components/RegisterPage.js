d3b98b8b022a44eb7433ae4857efef7db34e67bc
vihabidre <vihabidre@ccs.neu.edu>
2018-04-01 19:17:55 -0400

    Modified Login and Logout Pages with Routes

--- PhaseC/PlagiarismDetector/msdproject-client/src/components/RegisterPage.js
+++ PhaseC/PlagiarismDetector/msdproject-client/src/components/RegisterPage.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import axios from 'axios'
+import {BrowserRouter as Router, Link, Route} from 'react-router-dom'
 
 class RegisterPage extends React.Component{
 
